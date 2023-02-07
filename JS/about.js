a9ae719d3fd2cbcbc74aa5063dbc34fb44ee6a84
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-02-07 22:21:07 +0300

    Create about page js file

---
+++ JS/about.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-nav');
+
+const nav = document.querySelector('.ham-cont');
+const xColor = document.querySelector('.hamburger');
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle('is-active');
+  mobileMenu.classList.toggle('is-active');
+  nav.classList.toggle('color');
+  xColor.classList.toggle('x-color');
+});
